3f0491142e13108f85bfe66e86d723d001db3ce4
Dawson <125399990+Daws98@users.noreply.github.com>
2023-03-03 09:15:44 -0700

    Newest version of ex12 with no weak errors

--- ex12.py
+++ ex12.py
@@ -5,6 +5,6 @@
 nums = [2, 5, 6, 8, 11]
 print("List of number trying square", nums)
 # Computation
-for i in nums:
-    squares = [i**2 for i in nums]
-print("List of numbers squared", squares)
+for i in range(len(nums)):
+    nums[i] = nums[i]**2
+print("List of numbers after squared", nums)
